53dbe7b698df0e9e42112b20ba5fd4e936055c19
vardanakoyan <varanakoyan@gmail.com>
2025-02-07 15:24:43 +0400

    feat: imporove config module configs, docker compose

--- src/common/config/config-module.config.ts
+++ src/common/config/config-module.config.ts
@@ -6,5 +6,10 @@ export const ConfigModuleConfig: ConfigModuleOptions = {
   envFilePath: '.env',
   validationSchema: joi.object({
     PORT: joi.string().required(),
+    DB_NAME: joi.string().required(),
+    DB_USER: joi.string().required(),
+    DB_PASSWORD: joi.string().required(),
+    DB_HOST: joi.string().required(),
+    DB_PORT: joi.string().required(),
   }),
 };
