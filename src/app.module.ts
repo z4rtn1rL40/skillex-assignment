a75f5393cc74da8e0b43925bd8409f5237019610
vardanakoyan <varanakoyan@gmail.com>
2025-02-07 21:17:48 +0400

    feat: remove test services, encrypt envs

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ConfigModuleConfig, KnexModuleConfig } from './common/config';
 import { KnexModule } from 'nest-knexjs';
-import { UsersService } from './users/users.service';
 
 @Module({
-  providers: [UsersService],
+  providers: [],
   controllers: [],
   imports: [
     ConfigModule.forRoot(ConfigModuleConfig),
