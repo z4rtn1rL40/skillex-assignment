82795eb7a7a984b28dfa7f672653f2948707756f
vardanakoyan <varanakoyan@gmail.com>
2025-02-07 19:59:25 +0400

    feat: improve knex config

--- src/common/config/knex-module.config.ts
+++ src/common/config/knex-module.config.ts
@@ -1,9 +1,10 @@
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { KnexModuleOptions } from 'nest-knexjs';
 
 export const KnexModuleConfig = {
   import: [ConfigModule],
   inject: [ConfigService],
-  useFactory: (configService: ConfigService) => ({
+  useFactory: (configService: ConfigService): KnexModuleOptions => ({
     config: {
       client: 'mysql',
       version: '5.7',
